01b44e2e223f8f4446b9d2fb20f42da6a0e80f2f
Shaja Azımı <110715621+SHJAx19@users.noreply.github.com>
2023-03-19 13:04:18 +0300

    Add improvmetns and proper indentation

--- 7 - Library Management System/src/Main.java
+++ 7 - Library Management System/src/Main.java
@@ -1,20 +1,38 @@
 
 import java.util.Scanner;
 
+/**
+ * @author Shja Azımı
+ *         This is the Main class for a library management program.
+ *         It provides a user interface to interact with the Library class.
+ */
 public class Main {
-
+    /**
+     * The main method of the program. It displays the menu and calls the
+     * corresponding methods of Library class
+     * based on user input.
+     * 
+     * @param args Command line arguments (not used)
+     */
     public static void main(String[] args) {
-
         menu(); // Keep your main clean :)
     }
 
+    /**
+     * Displays the menu and provides a way for the user to interact with the
+     * Library.
+     */
     public static void menu() {
-        Library library = new Library();
+        Library library = new Library(); // create a new Library object
         Scanner scanner = new Scanner(System.in);
         int choice;
+
+        // Display welcome message
         System.out.println("-----------------------------");
         System.out.println("    Welcome to the library");
         System.out.println("-----------------------------\n");
+
+        // Display menu until user exits
         do {
             System.out.println("---------MENU--------");
             System.out.println("1. Add Book");
@@ -23,10 +41,13 @@ public class Main {
             System.out.println("4. Display All Books");
             System.out.println("5. Exit");
             System.out.print("Enter your choice: ");
+
             choice = scanner.nextInt();
-            scanner.nextLine();
+            scanner.nextLine(); // consume the newline character
+
             switch (choice) {
-                case 1 -> {
+                case 1:
+                    // Prompt user for book information and add it to the library
                     System.out.print("Enter the title of the book: ");
                     String title = scanner.nextLine();
                     System.out.print("Enter the author of the book: ");
@@ -34,23 +55,42 @@ public class Main {
                     System.out.print("Enter the ISBN of the book: ");
                     String isbn = scanner.nextLine();
                     library.addBook(new Book(title, author, isbn));
-                }
-                case 2 -> {
+                    break;
+
+                case 2:
+                    // Prompt user for book title and remove it from the library
                     System.out.print("Enter the title of the book to be removed: ");
                     String removeTitle = scanner.nextLine();
                     library.removeBook(removeTitle);
-                }
-                case 3 -> {
+                    break;
+
+                case 3:
+                    // Prompt user for book title and search for it in the library
                     System.out.print("Enter the title of the book to be searched: ");
                     String searchTitle = scanner.nextLine();
                     library.searchBook(searchTitle);
-                }
-                case 4 -> library.displayAllBooks();
-                case 5 -> System.out.println("Exiting program.");
-                default -> System.out.println("Invalid choice. Please try again.");
+                    break;
+
+                case 4:
+                    // Display all books in the library
+                    library.displayAllBooks();
+                    break;
+
+                case 5:
+                    // Exit the program
+                    System.out.println("Exiting program.");
+                    break;
+
+                default:
+                    // Invalid choice. Ask user to try again.
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
             }
         } while (choice != 5);
+
         scanner.close();
+
+        // Display goodbye message
         System.out.println("--------------------------------------");
         System.out.println("   Have a nice day. See you soon....");
         System.out.println("--------------------------------------");
